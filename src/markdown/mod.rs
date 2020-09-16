0b1c60382b66fe7fa1dd828d5fae7cae6d14f1fd
ondralukes <mail@ondralukes.cz>
2020-09-16 14:33:26 +0200

    Refactor

--- src/markdown/mod.rs
+++ src/markdown/mod.rs
@@ -16,146 +16,156 @@ impl<W: Write> Markdown<W> {
         self.writer
     }
 
-    pub fn write<T: MarkdownElement>(&mut self, element: T) -> Result<(), io::Error> {
-        element.write_to(&mut self.writer)?;
+    pub fn write<T: MarkdownWritable>(&mut self, element: T) -> Result<(), io::Error> {
+        element.write_to(&mut self.writer, false)?;
         Ok(())
     }
 }
 
-pub struct Heading<'a> {
-    value: StringBuffer<'a>,
-    level: usize,
+pub trait MarkdownWritable {
+    fn write_to(&self, writer: &mut dyn Write, inner: bool) -> Result<(), io::Error>;
 }
 
-impl<'a> Heading<'a> {
-    fn new(value: &'a str, level: usize) -> Self {
-        let mut buf = StringBuffer::new();
-        buf.append(value);
-        Self {
-            value: buf,
-            level,
-        }
-    }
-
-    pub fn append(&mut self, value: &'a str) -> &mut Self{
-        self.value.append(value);
-        self
-    }
+//region Paragraph
+pub struct Paragraph<'a>{
+    children: Vec<Box<dyn 'a + MarkdownWritable>>
 }
 
-impl MarkdownElement for &'_ mut Heading<'_>{
-    fn write_to<W: Write>(self, writer: &mut W) -> Result<(), io::Error>  {
-        let mut symbols = Vec::new();
-        symbols.resize(self.level, '#' as u8);
-        symbols.push(' ' as u8);
-
-        writer.write_all(&symbols)?;
-        self.value.write_to(writer)?;
-        writer.write_all(&['\n' as u8])?;
-        Ok(())
-    }
-}
-impl MarkdownElement for Heading<'_>{
-    fn write_to<W: Write>(mut self, writer: &mut W) -> Result<(), io::Error>  {
-        (&mut self).write_to(writer)
-    }
-}
-
-pub struct Paragraph<'a> {
-    value: StringBuffer<'a>,
-}
-
-impl<'a> Paragraph<'a> {
-    fn new(value: &'a str) -> Self {
-        let mut buf = StringBuffer::new();
-        buf.append(value);
-        Self {
-            value: buf
-        }
-    }
-
-    pub fn append(&mut self, value: &'a str) -> &mut Self{
-        self.value.append(value);
-        self
-    }
-}
-
-impl MarkdownElement for &'_ mut Paragraph<'_>{
-    fn write_to<W: Write>(self, writer: &mut W) -> Result<(), io::Error>  {
-        writer.write_all(&['\n' as u8])?;
-        self.value.write_to(writer)?;
-        writer.write_all(&['\n' as u8])?;
-        Ok(())
-    }
-}
-impl MarkdownElement for Paragraph<'_>{
-    fn write_to<W: Write>(mut self, writer: &mut W) -> Result<(), io::Error>  {
-        (&mut self).write_to(writer)
-    }
-}
-
-impl MarkdownElement for &str{
-    fn write_to<W: Write>(self, writer: &mut W) -> Result<(), Error> {
-        self.as_paragraph().write_to(writer)?;
-        Ok(())
-    }
-}
-
-impl MarkdownElement for String{
-    fn write_to<W: Write>(self, writer: &mut W) -> Result<(), Error> {
-        self.as_paragraph().write_to(writer)?;
-        Ok(())
-    }
-}
-
-pub trait MarkdownElement {
-    fn write_to<W: Write>(self, writer: &mut W) -> Result<(), io::Error>;
-}
-
-struct StringBuffer<'a>{
-    strings: Vec<&'a str>
-}
-
-impl<'a> StringBuffer<'a>{
-    fn new() -> Self{
+impl<'a> Paragraph<'a>{
+    pub fn new() -> Self{
         Self{
-            strings: Vec::new()
+            children: Vec::new()
         }
     }
 
-    fn append(&mut self, str: &'a str) {
-        self.strings.push(str);
+    pub fn append<T: 'a + MarkdownWritable>(&mut self, element: T) -> &mut Self{
+        self.children.push(Box::new(element));
+        self
     }
+}
 
-    fn write_to<W: Write>(&self, writer: &mut W) -> Result<(), io::Error>{
-        for s in &self.strings{
-            writer.write_all(s.as_bytes())?;
+impl MarkdownWritable for &'_ Paragraph<'_>{
+    fn write_to(&self, writer: &mut dyn Write, inner: bool) -> Result<(), Error> {
+        assert!(!inner);
+        writer.write_all(&['\n' as u8])?;
+        for child in &self.children{
+            child.write_to(writer, true)?;
         }
+        writer.write_all(&['\n' as u8])?;
         Ok(())
     }
 }
 
+impl MarkdownWritable for &'_ mut Paragraph<'_>{
+    fn write_to(&self, writer: &mut dyn Write, inner: bool) -> Result<(), Error> {
+        (&**self).write_to(writer, inner)
+    }
+}
+
+impl MarkdownWritable for Paragraph<'_>{
+    fn write_to(&self, writer: &mut dyn Write, inner: bool) -> Result<(), Error> {
+        (&self).write_to(writer, inner)
+    }
+}
+//endregion
+
+//region Heading
+pub struct Heading<'a>{
+    children: Vec<Box<dyn 'a + MarkdownWritable>>,
+    level: usize
+}
+
+impl<'a> Heading<'a>{
+    pub fn new(level: usize) -> Self{
+        assert!(level > 0 && level <= 6);
+        Self{
+            children: Vec::new(),
+            level
+        }
+    }
+
+    pub fn append<T: 'a + MarkdownWritable>(&mut self, element: T) -> &mut Self{
+        self.children.push(Box::new(element));
+        self
+    }
+}
+
+impl MarkdownWritable for &'_ Heading<'_>{
+    fn write_to(&self, writer: &mut dyn Write, inner: bool) -> Result<(), Error> {
+        assert!(!inner);
+        let mut prefix = Vec::new();
+        prefix.resize(self.level, '#' as u8);
+        prefix.push(' ' as u8);
+        writer.write_all(&prefix)?;
+        for child in &self.children{
+            child.write_to(writer, true)?;
+        }
+        writer.write_all(&['\n' as u8])?;
+        Ok(())
+    }
+}
+
+impl MarkdownWritable for &'_ mut Heading<'_>{
+    fn write_to(&self, writer: &mut dyn Write, inner: bool) -> Result<(), Error> {
+        (&**self).write_to(writer, inner)
+    }
+}
+
+impl MarkdownWritable for Heading<'_>{
+    fn write_to(&self, writer: &mut dyn Write, inner: bool) -> Result<(), Error> {
+        (&self).write_to(writer, inner)
+    }
+}
+//endregion
+
+//region String and &str
+impl MarkdownWritable for String{
+    fn write_to(&self, writer: &mut dyn Write, inner: bool) -> Result<(), io::Error> {
+        self.as_str().write_to(writer, inner)
+    }
+}
+
+impl MarkdownWritable for &str {
+    fn write_to(&self, writer: &mut dyn Write, inner: bool) -> Result<(), Error> {
+        if !inner {
+            writer.write_all(&['\n' as u8])?;
+        }
+        writer.write_all(self.as_bytes())?;
+        if !inner {
+            writer.write_all(&['\n' as u8])?;
+        }
+        Ok(())
+    }
+}
+//endregion
+
+//region AsMarkdown
 pub trait AsMarkdown {
-    fn as_heading(&self, level: usize) -> Heading;
     fn as_paragraph(&self) -> Paragraph;
+    fn as_heading(&self, level: usize) -> Heading;
 }
 
 impl AsMarkdown for String {
-    fn as_heading(&self, level: usize) -> Heading {
-        Heading::new(self, level)
+    fn as_paragraph(&self) -> Paragraph {
+        self.as_str().as_paragraph()
     }
 
-    fn as_paragraph(&self) -> Paragraph {
-        Paragraph::new(self)
+    fn as_heading(&self, level: usize) -> Heading {
+        self.as_str().as_heading(level)
     }
 }
 
-impl AsMarkdown for &str {
-    fn as_heading(&self, level: usize) -> Heading {
-        Heading::new(self, level)
+impl AsMarkdown for str {
+    fn as_paragraph(&self) -> Paragraph {
+        let mut p = Paragraph::new();
+        p.append(self);
+        p
     }
 
-    fn as_paragraph(&self) -> Paragraph {
-        Paragraph::new(self)
+    fn as_heading(&self, level: usize) -> Heading {
+        let mut h = Heading::new(level);
+        h.append(self);
+        h
     }
 }
+//endregion
